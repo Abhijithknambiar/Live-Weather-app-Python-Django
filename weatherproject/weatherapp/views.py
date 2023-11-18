85b84384df2ceab2d1f3d16e968818ab85d0a747
Abhijithknambiar <abhijithknambiar@outlook.com>
2023-11-18 20:27:17 +0530

    adding open weather api to view.py

--- weatherproject/weatherapp/views.py
+++ weatherproject/weatherapp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+import requests
+import datetime
 
-# Create your views here.
+
+def home(request):
+    if  'city' in request.POST:
+        city= request.POST['city']
+    else:
+        city='Bangalore'
+    url= f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=93fe1764b88529b8035bd5f6754f8ea5'
+    PARAMS= {'units';'metric'}
+    
+    return render(request,'weatherapp/index.html')
--- weatherproject/weatherproject/settings.py
+++ weatherproject/weatherproject/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    'weatherapp',
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
