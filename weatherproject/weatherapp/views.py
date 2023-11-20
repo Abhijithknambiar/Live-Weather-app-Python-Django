ed85ac35f78c202616b9ee1e1909169cefc549d0
Abhijithknambiar <abhijithknambiar@outlook.com>
2023-11-20 18:53:20 +0530

    index and css

--- weatherproject/weatherapp/views.py
+++ weatherproject/weatherapp/views.py
@@ -1,14 +1,23 @@
 from django.shortcuts import render
-import requests
+from django.contrib import messages
+#import request
 import datetime
 
 
 def home(request):
+
     if  'city' in request.POST:
         city= request.POST['city']
     else:
         city='Bangalore'
+
     url= f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=93fe1764b88529b8035bd5f6754f8ea5'
-    PARAMS= {'units';'metric'}
+    PARAMS= {'units':'metric'}
     
-    return render(request,'weatherapp/index.html')
+    data = request.get(url,params=PARAMS).json()
+    description = data['weather'][0]['description']
+    icon = data['weather'][0]['icon']
+    temp = data['main']['temp']
+    day = datetime.date.today()
+
+    return render(request,'weatherapp/index.html' , {'description':description , 'icon':icon ,'temp':temp , 'day':day , 'city':city})
