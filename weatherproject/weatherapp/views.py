88a41463b7c06cabe1f34b72b73d91057d54d7ab
Abhijithknambiar <abhijithknambiar@outlook.com>
2023-11-21 16:31:51 +0530

    update

--- weatherproject/weatherapp/views.py
+++ weatherproject/weatherapp/views.py
@@ -14,10 +14,21 @@ def home(request):
     url= f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=93fe1764b88529b8035bd5f6754f8ea5'
     PARAMS= {'units':'metric'}
     
-    data = requests.get(url,params=PARAMS).json()
-    description = data['weather'][0]['description']
-    icon = data['weather'][0]['icon']
-    temp = data['main']['temp']
-    day = datetime.date.today()
+    try:
 
-    return render(request,'index.html' , {'description':description , 'icon':icon ,'temp':temp , 'day':day , 'city':city})
+        data = requests.get(url,params=PARAMS).json()
+   
+        description = data['weather'][0]['description']
+        icon = data['weather'][0]['icon']
+        temp = data['main']['temp']
+   
+        day = datetime.date.today()
+
+        return render(request,'index.html',{'description':description , 'icon':icon ,'temp':temp , 'day':day , 'city':city,'exception_occured':False})
+    
+    except:
+        exception_occured=True
+        messages.error(request,'Entered data is not available to API')
+        day=datetime.date.today()
+
+        return render(request,'index.html',{'description':'clear sky' , 'icon':'01d' ,'temp':'25' , 'day':'day' , 'city':'bangalore','exception_occured':False,})
